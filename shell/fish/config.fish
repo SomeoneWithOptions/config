92bde433571ea67ce56faedaf451874f57f67893
SomeoneWithOptions <andrescastesol@gmail.com>
2026-09-13 22:12:27 -0500

    feat(shell): configure xai vertex ai credentials

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,3 +1,4 @@
+alias oq='omarchy-qemu'
 alias t='terraform'
 set -g fish_greeting ""
 
@@ -13,3 +14,7 @@ if test -f ~/.pi/agent/auth.json
     end
     set -e _openrouter_key
 end
+
+# xAI Grok on Vertex via service-account JSON, no gcloud login needed.
+set -gx GOOGLE_APPLICATION_CREDENTIALS /home/andres/it-tools-4fc1b6dd-9f8e204c0503.json
+set -gx XAI_VERTEX_PROJECT_ID it-tools-4fc1b6dd
